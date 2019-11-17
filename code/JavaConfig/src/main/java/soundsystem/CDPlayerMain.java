6f6ec23e61847eac113babca5c9eb43594aaa56f
aaachuan <2474333700@qq.com>
2019-11-17 20:53:27 +0800

    Add files via upload

---
+++ code/JavaConfig/src/main/java/soundsystem/CDPlayerMain.java
@@ -0,0 +1,16 @@
+package soundsystem;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CDPlayerMain {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(soundsystem.CDPlayerConfig.class);
+	    
+	    CompactDisc cd = context.getBean(CompactDisc.class);
+        cd.play();
+         
+	}
+
+}

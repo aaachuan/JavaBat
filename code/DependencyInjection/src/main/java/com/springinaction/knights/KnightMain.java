a58651ae246d02932af12166db687ac203fe9bc9
aaachuan <2474333700@qq.com>
2019-11-20 20:31:16 +0800

    Add files via upload

--- code/DependencyInjection/src/main/java/com/springinaction/knights/KnightMain.java
+++ code/DependencyInjection/src/main/java/com/springinaction/knights/KnightMain.java
@@ -1,11 +1,14 @@
 package com.springinaction.knights;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class KnightMain {
 
-	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knights.xml");
+	public static void main(String[] args) throws Exception{
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knights.xml");
+	    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(com.springinaction.knights.KnightConfig.class); 
 		Knight knight = context.getBean(Knight.class);
 		knight.embarkOnQuest();
 		 context.close();
